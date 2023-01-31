16bd949da5bc7c35defc47d290d94d583628872f
azmialwerfalli <drama.com.aa@gmail.com>
2023-01-31 19:33:10 +0200

    modified:   public/global.css 	deleted:    src/+layout.svelte 	modified:   src/App.svelte 	deleted:    src/about/+page.svelte 	new file:   src/components/Card.svelte 	new file:   src/components/FeedbackItem.svelte 	new file:   src/components/FeedbackList.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,60 +1,39 @@
 <script>
-	 let firstName = 'Azmi';
-	 let lastName = 'Alfoghi'
+	import FeedbackList from "./components/FeedbackList.svelte";
 
-	let color = 'blue'
-	let showText = false
-	let users  = [
+	let feedback = [
 		{
-			id: '1',
-			name: 'Azmi'
+			id: 1,
+			rating: 10,
+			text:  'some text for testing pourposes only please dont read me '
 		},
 		{
-			id: '2',
-			name: 'Ezzo'
+			id: 2,
+			rating: 6,
+			text:  'some text for testing pourposes only please dont read me '
 		},
 		{
-			id: '3',
-			name: 'Sarah'
-		}
+			id: 3,
+			rating: 9,
+			text:  'some text for testing pourposes only please dont read me '
+		},
+		{
+			id: 4,
+			rating: 2,
+			text:  'some text for testing pourposes only please dont read me '
+		},
 	]
-	 $: name = firstName + ' ' + lastName
-	 const toggle = () => {
-		color = color === 'blue' ? 'red' : 'blue'
-		showText = !showText
-		users = [...users, {id: '4', name:'jalal'}]
-	 }
+
+	$: count = feedback.length
+	$: average = feedback.reduce((a, {rating}) =>a + rating, 0) / feedback.length
+	const deleteFeedback = (e) =>{
+		const itemId = e.detail
+		feedback = feedback.filter((item) => item.id != itemId )
+	}
 </script>
 
-<main>
-	<h1 style="color: {color}">Hello {name}!</h1>
-	{#if showText}
-	<p>Some text Some text Some text Some text Some text .</p>
-	{/if}
-	<button on:click={toggle}>Click</button>
-	{#each users as user (user.id)}
-		<h3>{user.id}: {user.name}</h3>
-	{/each}
+<main class="container">
+	<h1>{average}</h1>
+<FeedbackList {feedback}  on:delete-feedback={deleteFeedback}/>
+
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,41 @@
+<script>
+    import {createEventDispatcher} from 'svelte'
+    import Card from "./Card.svelte";
+    export let item 
+    const dispatch = createEventDispatcher()
+    const handleDelete = (itemId) => {
+        dispatch('delete-feedback', itemId)
+    }
+</script>
+
+    <Card>
+        <div class="num-display">
+            {item.rating}
+        </div>
+        <button class="close" on:click={()=> handleDelete (item.id)}>X</button>
+        <div class="text-display">
+            {item.text}
+        </div>
+    </Card>
+    <style>
+        .num-display {
+            position: absolute;
+            inset: -10px auto auto -10px;
+            width: 50px;
+            height: 50px;
+            background: #ff6aa8;
+            color: #fff;
+            border: 1px #eee solid;
+            border-radius: 50%;
+            padding: 10px;
+            text-align: center;
+            font-size: 1.1rem;
+        }
+        .close {
+            position: absolute;
+            inset: 10px 20px auto auto;
+            cursor: pointer;
+            background: none;
+            border: none;
+        }
+    </style>
